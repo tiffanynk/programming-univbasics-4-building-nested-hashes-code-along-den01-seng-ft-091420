24f078897f789037b55b34b9c9c18efaa00099d3
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-08 22:31:54 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -5,10 +5,10 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_fiends => {}
-      }, 
+      },
     :capulet => {
       #your key/value pairs here!
       }}
 
-  
+
 end
